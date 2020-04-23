8a87d1f696eb92c03dbf8058d5d4e16d14fb603c
Pattaro <39515647+Pattaro@users.noreply.github.com>
2020-04-23 14:52:59 +0800

    train.py: nnc未加入训练

---
+++ Net_gpuVer3.0/core/evaluate.py
@@ -0,0 +1,65 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import torch
+import numpy as np
+from torch.autograd import Variable
+
+def pixelAccuracy(confusionMatrix):
+    # return all class overall pixel accuracy
+    # acc = (TP + TN) / (TP + TN + FP + TN)
+    acc = np.diag(confusionMatrix).sum() / confusionMatrix.sum()
+    return acc
+
+def meanIntersectionOverUnion(confusionMatrix):
+    # Intersection = TP Union = TP + FP + FN
+    # IoU = TP / (TP + FP + FN)
+    intersection = np.diag(confusionMatrix)
+    union = np.sum(confusionMatrix, axis=1) + np.sum(confusionMatrix, axis=0) - np.diag(
+        confusionMatrix)
+    IoU = intersection / union
+    mIoU = np.nanmean(IoU)
+    return mIoU
+
+def genConfusionMatrix(imgPredict, imgLabel, numClass):
+
+    # remove classes from unlabeled pixels in gt image and predict
+    mask = (imgLabel >= 0) & (imgLabel < numClass)
+    label = numClass * imgLabel[mask] + imgPredict[mask]
+    count = np.bincount(label, minlength=numClass ** 2)
+    confusionMatrix = count.reshape(numClass, numClass)
+    return confusionMatrix
+
+
+def accuracy(label_preds, label_trues):
+
+    label_preds[label_preds >= 0.5] = 1
+    label_preds[label_preds < 0.5] = 0
+    label_trues[label_trues >= 0.5] = 1
+    label_trues[label_trues < 0.5] = 0
+
+    # Variable -> Tensor -> numpy -> int
+    label_preds = label_preds.cpu().data.numpy().astype(int)
+    label_trues = label_trues.cpu().data.numpy().astype(int)
+
+    # 0,1
+    numClass = 2
+    with torch.no_grad():
+        confusionMatrix = np.zeros((numClass,) * 2)
+
+        assert label_preds.shape == label_trues.shape
+        confusionMatrix += genConfusionMatrix(label_preds, label_trues, numClass)
+
+        acc = pixelAccuracy(confusionMatrix)
+        mIoU =  meanIntersectionOverUnion(confusionMatrix)
+
+    return acc, mIoU
+
+if __name__ == '__main__':
+    imgPredict = Variable(torch.FloatTensor([[[[0., 0., 1., 1., 0.6, 0.]]]]))
+    imgLabel = Variable(torch.FloatTensor([[[[0., 0., 1., 1., 1., 1.]]]]))
+
+    acc, miou = accuracy(imgLabel, imgPredict)
+
+    print(acc,miou)
---
+++ Net_gpuVer3.0/valid.py
@@ -0,0 +1,121 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import os
+import sys
+import shutil
+import pprint
+
+import torch
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import torch.utils.data.distributed
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+import torchvision
+import numpy as np
+np.set_printoptions(threshold=np.inf)
+import matplotlib.pyplot as plt
+
+import dataset
+import models
+from config import config
+from config import update_config
+from utils.modelsummary import get_model_summary
+from utils.utils import create_logger
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train keypoints network')
+
+    parser.add_argument('--cfg',
+                        help='experiment configure file name',
+                        type=str,
+                        default='./nnb_adam_lr5e-2_bs32.yaml')
+
+    parser.add_argument('--modelDir',
+                        help='model directory',
+                        type=str,
+                        default='')
+    parser.add_argument('--logDir',
+                        help='log directory',
+                        type=str,
+                        default='')
+    parser.add_argument('--dataDir',
+                        help='data directory',
+                        type=str,
+                        default='')
+    parser.add_argument('--testModel',
+                        help='testModel',
+                        type=str,
+                        default='./output/BI_dataset/faceXray.pth')
+
+    args = parser.parse_args()
+    update_config(config, args)
+
+    return args
+
+
+def main():
+    args = parse_args()
+
+    # cudnn related setting
+    cudnn.benchmark = config.CUDNN.BENCHMARK
+    torch.backends.cudnn.deterministic = config.CUDNN.DETERMINISTIC
+    torch.backends.cudnn.enabled = config.CUDNN.ENABLED
+
+    model = eval('models.' + config.MODEL.NAME + '.get_nnb')(
+        config)
+
+    gpus = list(config.GPUS)
+    model = torch.nn.DataParallel(model, device_ids=gpus).cuda()
+
+    # Data loading code
+    valid_dataset = eval('dataset.' + config.DATASET.DATASET + '.' + config.DATASET.DATASET)(
+        config.DATASET.ROOT, config.DATASET.TEST_SET, None,
+        transforms.Compose([
+            transforms.Resize(256),
+            transforms.ToTensor()
+        ])
+    )
+
+    valid_loader = torch.utils.data.DataLoader(
+        valid_dataset,
+        batch_size=config.TEST.BATCH_SIZE_PER_GPU*len(gpus),
+        shuffle=False,
+        num_workers=config.WORKERS,
+        pin_memory=config.PIN_MEMORY
+    )
+
+    model.eval()
+
+    with torch.no_grad():
+        for i, (input, target, _, _) in enumerate(valid_loader):
+
+            # # 输出label
+            target = torchvision.utils.make_grid(target).numpy()
+            target = np.transpose(target, (1, 2, 0))
+
+            plt.imshow(target)
+            plt.show()
+            plt.pause(0.5)
+
+            # 输出网络输出
+            output = model(input)
+
+            output = torchvision.utils.make_grid(output).numpy()
+            output = np.transpose(output, (1, 2, 0))
+
+            plt.imshow(output)
+            plt.show()
+            plt.pause(0.5)
+
+
+
+
+if __name__ == '__main__':
+    main()
